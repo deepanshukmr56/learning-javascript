ab10518cd7312a7feb6eefca576c4aa052ad8d83
deepanshukmr56 <144052154+deepanshukmr56@users.noreply.github.com>
2024-02-13 12:22:40 +0000

    conversion of datatypes

---
+++ 01_BASICS/03_conversion.js
@@ -0,0 +1,27 @@
+// let aNumber= 33
+
+// let aString= String(aNumber)
+//as per above code the typeof was string and string is 33
+
+
+
+// let aNumber= 1
+
+// let tommorowIsFullMoon= Boolean(aNumber)
+// as per above code the type is boolean and the output is True
+
+
+// let answer = undefined
+
+// let container= Number(answer)
+// from the above code the type converted to "number" but the output was nan(not a number)
+
+
+// let answer = null
+
+// let container= Number(null)
+//from the above code the null converted to number and the value is 0.
+
+console.log(typeof(container));
+console.log(container);
+
