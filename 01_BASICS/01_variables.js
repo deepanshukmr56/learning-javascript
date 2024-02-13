66f7265bfb85f7d4c9084e821bb56ee84c883738
deepanshukmr56 <144052154+deepanshukmr56@users.noreply.github.com>
2024-02-13 07:38:21 +0000

    let, var and const concepts

--- 01_BASICS/01_variables.js
+++ 01_BASICS/01_variables.js
@@ -1,6 +1,6 @@
 const userId= 2134715
 let userName= "deepanshu"
-var password= "12121212"
+var password= "12121212"//we can use var in place of let but prefer not to use because of issue in block scope and functional scope
 userCity= "chandigarh"//you can also use this way to declare a varible but this is not a recommended practice.
 var userState //the userState have no value in it and thats why it is giving "undefined" as output.
 // userId= 2134722
