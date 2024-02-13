6b610b652cea71abda64bc1dc267c9510b52f2e3
deepanshukmr56 <144052154+deepanshukmr56@users.noreply.github.com>
2024-02-13 07:32:48 +0000

    learn var, let, const

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,11 @@
+const userId= 2134715
+let userName= "deepanshu"
+var password= "12121212"
+userCity= "chandigarh"//you can also use this way to declare a varible but this is not a recommended practice.
+var userState //the userState have no value in it and thats why it is giving "undefined" as output.
+// userId= 2134722
+// userId is a constant and cannot be updated and show error "assignment to be contant variable"
+userName= "kunal"
+userCity= "faridkot"
+password= "12121213"
+console.table([userId,userName,password,userCity,userState])
