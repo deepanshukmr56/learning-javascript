1011a6fea1c703a6677c5fbac848f5118d23b143
deepanshukmr56 <144052154+deepanshukmr56@users.noreply.github.com>
2024-02-14 06:53:28 +0000

    datatype comparison

---
+++ 01_BASICS/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+//these above types are normal comparisons, gives output in true and false and a recommended practice
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//in the case og null the js sometimes treat as nan and sometime as 0.
+
+
+// console.log("2" > 1);
+// console.log("2" < 1);
+
+//in javascript these answer are unpredictable and thats why these are not a recommended practice
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+
