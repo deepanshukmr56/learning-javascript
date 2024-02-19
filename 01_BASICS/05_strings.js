0109a971e87db5ab5a43c86c5e491aa1ae2ca5a8
deepanshukmr56 <144052154+deepanshukmr56@users.noreply.github.com>
2024-02-19 06:17:58 +0000

    strings methods

---
+++ 01_BASICS/05_strings.js
@@ -0,0 +1,17 @@
+const name = "deepanshu "
+const mobileNo = 8484848484
+
+// console.log(name + mobileNo);
+//the above concatenation process is valid but sometimes it become chaotic to write and understand and also gives unvalid outputs
+
+
+
+// console.log(`my name is ${name}and my mobile number is ${mobileNo}`);
+//the above type is called as string interpolation and it is good practice as well as make code more readable
+
+
+const gameName = new String ("clash of clans")
+console.log(gameName.charAt(7));//this charat method is used to print character available at given index no.
+console.log(gameName[7]);//this also gives the same output as charat
+console.log(gameName.charCodeAt(10));// String values returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
+console.log(gameName
