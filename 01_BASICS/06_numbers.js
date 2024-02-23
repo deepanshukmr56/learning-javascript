1e3a4f11a365f53f69484e324b55ba01ffd2fa7e
deepanshukmr56 <144052154+deepanshukmr56@users.noreply.github.com>
2024-02-23 10:51:02 +0000

    arrays and objects

---
+++ 01_BASICS/06_numbers.js
@@ -0,0 +1,2 @@
+const pin = new Number(2001)
+console.log(Number);
---
+++ 02_BASICS/01_arrays.js
@@ -0,0 +1,13 @@
+const firstArray = [1, 24, 54, 69, 130]
+
+
+
+// console.log(firstArray.at(5));
+//.at method is used to print the item available on the index we entered and allows both positive and negative integers. and Always returns undefined if the the index is invalid. The at() method is equivalent to array[n]
+
+
+// const twoArray = [45, 64, 10, 50, 4]
+// const thirdArray = [12, 14, 0, 65, 56]
+// const newArray = firstArray.concat(twoArray, thirdArray)
+// console.log(newArray);
+// .concat method is used to concat two or more array but always that the concatenated value stored into a new variable. This method does not change the existing arrays, but instead returns a new array.
