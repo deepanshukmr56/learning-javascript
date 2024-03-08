a6a64139667881798f48aef70125bd4f071cd4c3
deepanshukmr56 <144052154+deepanshukmr56@users.noreply.github.com>
2024-03-08 09:27:16 +0000

    iteration for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,26 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(`this is ${i}`);
+    
+// }
+// the above code is a basic structure of for loop in javascript and the for loop also follows the scoping and creates block scope which means variables declared in the for loop are not accessible outside of for loop
+
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i ;
+//     console.log(`table of ${i}`);
+//     for (let j = 1; j < 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+
+//     }
+    
+// }
+// the above code is the example of a nested for loop and we are printing table of 1 to 10 here
+
+
+
+
